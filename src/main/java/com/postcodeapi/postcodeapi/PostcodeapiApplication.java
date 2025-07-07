528b900baca72f9b30b9d596bb311a1eed3e664e
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-07-07 17:33:28 +1000

    initial commit

---
+++ src/main/java/com/postcodeapi/postcodeapi/PostcodeapiApplication.java
@@ -0,0 +1,13 @@
+package com.postcodeapi.postcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postcodeapi/postcodeapi/PostcodeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postcodeapi.postcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
